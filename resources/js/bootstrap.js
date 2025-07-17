e03fd885e1952f3f149e2e5f91a0240bd2b0c4b4
Mark <16464861+Markyiptw@users.noreply.github.com>
2025-07-17 15:04:16 +0000

    Setup alpine and htmx.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+import htmx from "htmx.org";
+window.htmx = htmx;
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
