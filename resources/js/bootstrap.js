5a93f256fce2f99d0c82723af3c7752e5c1601e9
Mark <16464861+Markyiptw@users.noreply.github.com>
2025-07-19 04:48:29 +0000

    Setup hx-target-error

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,6 +6,8 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 import htmx from "htmx.org";
 window.htmx = htmx;
 
+import "htmx-ext-response-targets";
+
 import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,6 +23,8 @@
         @endif
     </head>
     <body
+        hx-ext="response-targets"
+        hx-target-error="this"
         class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]"
     >
         {{ $slot }}
