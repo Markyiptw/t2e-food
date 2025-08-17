3f2142932bea5169cca182bda5afca45289740c1
Mark <16464861+Markyiptw@users.noreply.github.com>
2025-08-17 07:03:55 +0000

    Try fixing settings.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware
-            ->trustProxies(['kamal-proxy']);
+            ->trustProxies(
+                ['kamal-proxy'],
+                Request::HEADER_X_FORWARDED_PROTO
+            );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
