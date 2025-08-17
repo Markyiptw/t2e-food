c70fa726ef62334565c9569f75f9cba6b669160f
Mark <16464861+Markyiptw@users.noreply.github.com>
2025-08-17 06:51:47 +0000

    See if adding trust proxies solve content csp.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware
+            ->trustProxies(['kamal-proxy']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
