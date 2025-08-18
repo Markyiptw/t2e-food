85ab9855cae7d9903b151e38f7e99084f6b86437
Mark <16464861+Markyiptw@users.noreply.github.com>
2025-08-18 03:43:14 +0000

    Setup proxy again with sightly changes.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\LogRequestHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware
+            ->trustProxies('*', Request::HEADER_X_FORWARDED_PROTO)
             ->append(LogRequestHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
