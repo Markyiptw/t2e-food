89dcaeb3484938fb6f729c5b18d1aa3a2ed1c0f0
Mark <16464861+Markyiptw@users.noreply.github.com>
2025-08-18 03:31:06 +0000

    Log request for debug reverse proxy setup.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Middleware\LogRequestHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,10 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware
-            ->trustProxies(
-                ['*'],
-                Request::HEADER_X_FORWARDED_TRAEFIK
-            );
+            ->append(LogRequestHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
