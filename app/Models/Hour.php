75c56f6812812e3bd5b7bbb356210842d5be29e3
Mark <16464861+Markyiptw@users.noreply.github.com>
2026-05-07 09:20:20 +0000

    add relationship.

--- app/Models/Hour.php
+++ app/Models/Hour.php
@@ -4,6 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['restaurant_id', 'data'])]
-class Hour extends Model {}
+class Hour extends Model
+{
+    public function periods(): HasMany
+    {
+        return $this->hasMany(Period::class);
+    }
+}
