5e0298fd5a44cd2034b416d235a20b1ee98db948
Mark <16464861+Markyiptw@users.noreply.github.com>
2026-05-12 05:07:32 +0000

    homepage by ai.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,10 +1,12 @@
+@props(['title' => 'Time 2 Eat'])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Laravel</title>
+        <title>{{ $title ?? 'Time 2 Eat' }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />
