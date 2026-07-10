f011625507ebe8e54b34fef5bdbdda3f1d89eabd
Markyiptw <16464861+Markyiptw@users.noreply.github.com>
2026-07-10 09:29:05 +0000

    Fix marker not loading once asset get bundled

--- resources/js/map.js
+++ resources/js/map.js
@@ -2,6 +2,16 @@ import L from "leaflet";
 import "leaflet.markercluster";
 import "leaflet.markercluster/dist/MarkerCluster.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 
 const mapContainer = document.getElementById("map");
 
